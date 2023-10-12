86802ca6e5853f25ca04ff904b7f170d0a87a643
DunderAlexander <mego.sanello@gmail.com>
2023-10-12 10:35:24 +0700

    header and footer html layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,47 @@
-const Footer = () => {
-  return <footer>Footer</footer>;
+interface FooterItem {
+  alias: string;
+  pagetitle: string;
+  menutitle: string;
+  uri: string;
+}
+interface Social {
+  instagram: string;
+  linkedin: string;
+  twitter: string;
+  youtube: string;
+}
+
+type Footer = FooterItem[];
+
+const Footer = ({
+  footer,
+  social,
+  sitename,
+}: {
+  footer: Footer;
+  social: Social;
+  sitename: string;
+}) => {
+  return (
+    <footer>
+      <div>
+        <h1>{sitename}</h1>
+      </div>
+      <div>
+        <ul>
+          <li>GMP © 2023</li>
+          <li>Insta</li>
+          <li>LinkedIn</li>
+          <li>Twitter</li>
+          <li>YouTube</li>
+        </ul>
+        <ul>
+          {footer.map((item) => (
+            <li key={item.alias}>{item.pagetitle}</li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,27 @@
-const Header = () => {
-  return <header>Header</header>;
+interface MenuItem {
+  alias: string;
+  pagetitle: string;
+  menutitle: string;
+  uri: string;
+}
+
+type Menu = MenuItem[];
+
+const Header = ({ menu }: { menu: Menu }) => {
+  return (
+    <header>
+      <img src="/einzelwerk.svg" alt="logo" />
+      <div>
+        <ul>
+          {menu.map((menuItem) => (
+            <li key={menuItem.pagetitle}>{menuItem.pagetitle}</li>
+          ))}
+        </ul>
+        <div>lang_picker_placeholder</div>
+      </div>
+      <button>Contact Us</button>
+      <img src="/menu.svg" alt="menu" />
+    </header>
+  );
 };
 export default Header;
