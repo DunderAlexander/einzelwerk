598bbcd6c3c76be3acb70ad3d71c5934c13a3b8e
DunderAlexander <mego.sanello@gmail.com>
2023-10-15 10:10:40 +0700

    header uses svgr icons now

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import Logo from "../public/einzelwerk.svg?svgr";
+import BurgerMenu from "../public/menu.svg?svgr";
+
 interface MenuItem {
   alias: string;
   pagetitle: string;
@@ -9,8 +12,8 @@ type Menu = MenuItem[];
 
 const Header = ({ menu }: { menu: Menu }) => {
   return (
-    <div className="flex justify-between px-4 py-2 mt-4 w-full rounded-3xl bg-header-background text-white">
-      <img src="/einzelwerk.svg" alt="logo" />
+    <div className="flex justify-between items-center px-4 py-2 mt-4 w-full rounded-3xl bg-header-background text-white">
+      <Logo />
       <div className="hidden md:flex items-center gap-4">
         <ul className="flex gap-4">
           {menu.map((menuItem) => (
@@ -21,7 +24,7 @@ const Header = ({ menu }: { menu: Menu }) => {
       <div className="flex gap-4 items-center">
         <div className="hidden">lang_picker_placeholder</div>
         <button className="bg-button py-3 px-4 rounded-2xl">Contact us</button>
-        <img className="md:hidden" src="/menu.svg" alt="menu" />
+        <BurgerMenu className="md:hidden" />
       </div>
     </div>
   );
